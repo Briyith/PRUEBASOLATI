668f3c9dc831497baa0b76435212a78250d49294
Briyith <dayana01diaz@gmail.com>
2024-01-25 22:44:58 -0500

    first commit

---
+++ BACK/src/main/java/com/solati/crud/security/service/RolService.java
@@ -0,0 +1,26 @@
+package com.solati.crud.security.service;
+
+import com.solati.crud.security.entity.Rol;
+import com.solati.crud.security.enums.RolNombre;
+import com.solati.crud.security.repository.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return rolRepository.findByRolNombre(rolNombre);
+    }
+
+    public void save(Rol rol){
+        rolRepository.save(rol);
+    }
+}
---
+++ BACK/src/main/java/com/solati/crud/util/CreateRoles.java
@@ -0,0 +1,31 @@
+package com.solati.crud.util;
+
+import com.solati.crud.security.entity.Rol;
+import com.solati.crud.security.enums.RolNombre;
+import com.solati.crud.security.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * MUY IMPORTANTE: ESTA CLASE SÓLO SE EJECUTARÁ UNA VEZ PARA CREAR LOS ROLES.
+ * UNA VEZ CREADOS SE DEBERÁ ELIMINAR O BIEN COMENTAR EL CÓDIGO
+ *
+ */
+
+@Component
+public class CreateRoles implements CommandLineRunner {
+
+    @Autowired
+    RolService rolService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        /** Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser); **/
+
+    }
+}
